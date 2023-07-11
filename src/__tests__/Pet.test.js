e9aae047b3a2d20d658827c910179745a508886a
Murilo <murilo.ferreira@academico.ifpb.edu.br>
2023-07-11 15:08:00 -0300

    Criando teste para a função de ler os 5 primeiros

--- src/__tests__/Pet.test.js
+++ src/__tests__/Pet.test.js
@@ -1,8 +1,8 @@
-const Pet = require('../models/Pet');
+const Pet = require("../models/Pet");
 
-describe('Serviço de pets', () => {
-  test('Deve ser possível adicionar um pet', async () => {
-    const pet = { nome: 'Fuleco', tutor: 'Eli', telefone: '8394584656', endereco: 'Sitio Saco' };
+describe("Serviço de pets", () => {
+  test("Deve ser possível adicionar um pet", async () => {
+    const pet = { nome: "Fuleco", tutor: "Eli", telefone: "8394584656", endereco: "Sitio Saco" };
 
     const petsAntes = await Pet.readAll();
     const qntPetsAntes = petsAntes.length;
@@ -15,15 +15,15 @@ describe('Serviço de pets', () => {
     expect(qntPetsDepois).toBe(qntPetsAntes + 1);
   });
 
-  test('Deve ser possível pesquisar um pet pelo nome', async () => {
-    const nome = 'Fuleco';
+  test("Deve ser possível pesquisar um pet pelo nome", async () => {
+    const nome = "Fuleco";
     const pet = await Pet.search(nome);
 
     expect(pet).toBeDefined();
     expect(pet.some((pet) => pet.nome === nome)).toBe(true);
   });
 
-  test('Deve ser possível encontrar um pet pelo id', async () => {
+  test("Deve ser possível encontrar um pet pelo id", async () => {
     const pets = await Pet.readAll();
     const indice = pets.length - 1;
     const id = pets[indice].id;
@@ -34,16 +34,16 @@ describe('Serviço de pets', () => {
     expect(pet.id).toBe(id);
   });
 
-  test('Deve ser possível atualizar um pet através do id', async () => {
+  test("Deve ser possível atualizar um pet através do id", async () => {
     const pets = await Pet.readAll();
     const indice = pets.length - 1;
     const id = pets[indice].id;
 
     const pet = {
-      nome: 'Caramelo',
-      tutor: 'Shelby',
-      telefone: '834545932',
-      endereco: 'Terra de Ninguém',
+      nome: "Caramelo",
+      tutor: "Shelby",
+      telefone: "834545932",
+      endereco: "Terra de Ninguém",
     };
 
     const update = await Pet.update(id, pet);
@@ -51,7 +51,7 @@ describe('Serviço de pets', () => {
     expect(update).toBe(1);
   });
 
-  test('Deve ser possível deletar um pet através do id', async () => {
+  test("Deve ser possível deletar um pet através do id", async () => {
     const pets = await Pet.readAll();
     const indice = pets.length - 1;
     const id = pets[indice].id;
@@ -62,12 +62,12 @@ describe('Serviço de pets', () => {
   });
 
   test("Devem ser retornados 5 pets ou menos com o comando readByName", async () => {
-    const pet1 = { nome: 'Pet1', tutor: 'Tutor1', telefone: 'Telefone1', endereco: 'Endereço1' };
-    const pet2 = { nome: 'Pet2', tutor: 'Tutor2', telefone: 'Telefone2', endereco: 'Endereço2' };
-    const pet3 = { nome: 'Pet3', tutor: 'Tutor3', telefone: 'Telefone3', endereco: 'Endereço3' };
-    const pet4 = { nome: 'Pet4', tutor: 'Tutor4', telefone: 'Telefone4', endereco: 'Endereço4' };
-    const pet5 = { nome: 'Pet5', tutor: 'Tutor5', telefone: 'Telefone5', endereco: 'Endereço5' };
-    const pet6 = { nome: 'Pet6', tutor: 'Tutor6', telefone: 'Telefone6', endereco: 'Endereço6' };
+    const pet1 = { nome: "Pet1", tutor: "Tutor1", telefone: "Telefone1", endereco: "Endereço1" };
+    const pet2 = { nome: "Pet2", tutor: "Tutor2", telefone: "Telefone2", endereco: "Endereço2" };
+    const pet3 = { nome: "Pet3", tutor: "Tutor3", telefone: "Telefone3", endereco: "Endereço3" };
+    const pet4 = { nome: "Pet4", tutor: "Tutor4", telefone: "Telefone4", endereco: "Endereço4" };
+    const pet5 = { nome: "Pet5", tutor: "Tutor5", telefone: "Telefone5", endereco: "Endereço5" };
+    const pet6 = { nome: "Pet6", tutor: "Tutor6", telefone: "Telefone6", endereco: "Endereço6" };
 
     const petCriado1 = await Pet.create(pet1);
     const petCriado2 = await Pet.create(pet2);
@@ -76,9 +76,14 @@ describe('Serviço de pets', () => {
     const petCriado5 = await Pet.create(pet5);
     const petCriado6 = await Pet.create(pet6);
 
-    const petsLidos = await Pet.readByName("Pet")
-    const qtdPets = petsLidos.length
+    const petsLidos = await Pet.readByName("Pet");
+    const qtdPets = petsLidos.length;
 
     expect(qtdPets).toBeLessThanOrEqual(5);
-  })
+  });
+  test("Devem ser possível listar até os 5 primeiros pets do banco com readFirst5", async () => {
+    const pets = await Pet.readFirst5();
+    const qtdPets = pets.length;
+    expect(qtdPets).toBeLessThanOrEqual(5);
+  });
 });
