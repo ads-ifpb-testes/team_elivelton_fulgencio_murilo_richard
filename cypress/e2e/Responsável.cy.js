151babf046caf81daae909d7f09a337b53dfd6f8
Elivelton Pereira <elivelton.pereira@academico.ifpb.edu.br>
2023-07-13 09:35:50 -0300

    Criando teste para cadastro de novo responsável

--- cypress/e2e/Responsável.cy.js
+++ cypress/e2e/Responsável.cy.js
@@ -3,7 +3,9 @@
 describe('template spec', () => {
   const host = 'http://localhost:8080';
 
-  beforeEach(() => cy.visit(host));
+  beforeEach(() => {
+    cy.visit(host)
+  });
 
   it('Deve exibir uma formulário para logar um responsável na conta e entrar na conta', () => {
     cy.get('#open-responsavel-page').click();
@@ -15,4 +17,26 @@ describe('template spec', () => {
 
     cy.url().should('eq', `${host}/home`);
   });
+  
+  it('Deve exibir uma formulário para cadastrar um novo responsável no sistema', () => {
+    cy.get('#open-responsavel-page').click();
+    cy.get('.form-login').should('exist');
+
+    cy.get('#login-email').type(Cypress.env('email'));
+    cy.get('#login-password').type(Cypress.env('senha'));
+    cy.get('#button-login').click();
+
+    cy.url().should('eq', `${host}/home`);
+    cy.get('#open-responsavel-page').click();
+
+    cy.get('#nomeRes').type('Dr. Hector Bonilha');
+    cy.get('#funcaoRes').type('Veterinário');
+    cy.get('#telefoneRes').type('129838293');
+    cy.get('#emailRes').type('hectorbolinha@gmail.com');
+    cy.get('#senhaRes').type('1234'); 
+    cy.get('#senhaResConf').type('1234');
+    cy.get('#enviarRes').click();
+    cy.get('#botaoVoltar').click();
+  });
+
 });
